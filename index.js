42168794123b8016dbea56c86646f6d47c16e808
shashank <shashank28042001@gmail.com>
2021-06-21 22:52:03 +0530

    final side-navbar commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-$('.feat-btn').click(function(){
-    $('nav ul .feat-show').toggleClass("show");
-    $('nav ul .first').toggleClass("rotate");
+$('.btn').click(function(){
+    $(this).toggleClass("click");
+    $('.sidebar').toggleClass("show");
   });
-
-  $('.serv-btn').click(function(){
-    $('nav ul .serv-show').toggleClass("show1");
-    $('nav ul .second').toggleClass("rotate");
-  });
-
-  $('.thir-btn').click(function(){
-      $('nav ul .thir-show').toggleClass("show2");
-      $('nav ul .third').toggleClass("rotate");
-  });
-  
+    $('.feat-btn').click(function(){
+      $('nav ul .feat-show').toggleClass("show");
+      $('nav ul .first').toggleClass("rotate");
+    });
+    $('.serv-btn').click(function(){
+      $('nav ul .serv-show').toggleClass("show1");
+      $('nav ul .second').toggleClass("rotate");
+    });
+    $('nav ul li').click(function(){
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+    $('.thir-btn').click(function(){
+        $('nav ul .thir-show').toggleClass("show2");
+        $('nav ul .third').toggleClass("rotate");
+    });

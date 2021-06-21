c4345179a8627c264f9a92a99ea880dcc01dc564
shashank <shashank28042001@gmail.com>
2021-06-21 22:34:18 +0530

    final js class addition pending

---
+++ index.js
@@ -0,0 +1,15 @@
+$('.feat-btn').click(function(){
+    $('nav ul .feat-show').toggleClass("show");
+    $('nav ul .first').toggleClass("rotate");
+  });
+
+  $('.serv-btn').click(function(){
+    $('nav ul .serv-show').toggleClass("show1");
+    $('nav ul .second').toggleClass("rotate");
+  });
+
+  $('.thir-btn').click(function(){
+      $('nav ul .thir-show').toggleClass("show2");
+      $('nav ul .third').toggleClass("rotate");
+  });
+  

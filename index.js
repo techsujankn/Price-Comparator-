e31da79799de61c41cd6cd4066db60290d6f9c67
shashank <shashank28042001@gmail.com>
2021-06-23 17:21:48 +0530

    final bothnavbar commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 $('.btn').click(function(){
-    $(this).toggleClass("click");
-    $('.sidebar').toggleClass("show");
+  $(this).toggleClass("click");
+  $('.sidebar').toggleClass("show");
+});
+  $('.feat-btn').click(function(){
+    $('.sidebar ul .feat-show').toggleClass("show");
+    $('.sidebar ul .first').toggleClass("rotate");
   });
-    $('.feat-btn').click(function(){
-      $('nav ul .feat-show').toggleClass("show");
-      $('nav ul .first').toggleClass("rotate");
-    });
-    $('.serv-btn').click(function(){
-      $('nav ul .serv-show').toggleClass("show1");
-      $('nav ul .second').toggleClass("rotate");
-    });
-    $('nav ul li').click(function(){
-      $(this).addClass("active").siblings().removeClass("active");
-    });
-    $('.thir-btn').click(function(){
-        $('nav ul .thir-show').toggleClass("show2");
-        $('nav ul .third').toggleClass("rotate");
-    });
+  $('.serv-btn').click(function(){
+    $('.sidebar ul .serv-show').toggleClass("show1");
+    $('.sidebar ul .second').toggleClass("rotate");
+  });
+  $('.sidebar ul li').click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+  });
+  $('.thir-btn').click(function(){
+      $('.sidebar ul .thir-show').toggleClass("show2");
+      $('.sidebar ul .third').toggleClass("rotate");
+  });
